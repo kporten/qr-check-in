479209b2b4ce9d54091b930bca3f1e631c5bcfed
Kevin Porten <hello@kevinporten.dev>
2020-08-08 21:02:30 +0200

    feat: add template engine pug and a better error handling

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -8,80 +8,84 @@ import * as User from '../models/user.js';
 
 const router = express.Router();
 
-router.get('/:name/register', async (request, response) => {
-  const name = request.params.name.toLowerCase();
+router.get('/register', async (request, response, next) => {
+  try {
+    if (!request.query.name) {
+      response.status(400).json({
+        type: 'invalid_request_error',
+        code: 'parameter_invalid_empty',
+        param: 'name',
+        message: '-name- is required (e.g. GET /users/register?name=john)',
+      });
 
-  let user = await User.get({ name });
+      return;
+    }
 
-  if (!user) {
-    user = await User.add({ name, token: nanoid(6) });
+    const name = request.query.name.toLowerCase();
+
+    let user = await User.get({ name });
+
+    if (!user) {
+      user = await User.add({ name, token: nanoid(6) });
+    }
+
+    const origin = url.format({
+      protocol: request.protocol,
+      host: request.get('host'),
+    });
+
+    const address = {
+      checkIn: `${origin}${request.baseUrl}/${user.token}/check-in`,
+      checkOut: `${origin}${request.baseUrl}/${user.token}/check-out`,
+    };
+
+    const [checkIn, checkOut] = await Promise.all([
+      qrcode.toDataURL(address.checkIn),
+      qrcode.toDataURL(address.checkOut),
+    ]);
+
+    response.render('users/register', {
+      user,
+      address,
+      qr: { checkIn, checkOut },
+    });
+  } catch (error) {
+    next(error);
   }
-
-  const origin = url.format({
-    protocol: request.protocol,
-    host: request.get('host'),
-  });
-
-  const checkInUrl = `${origin}${request.baseUrl}/${user.token}/check-in`;
-  const checkOutUrl = `${origin}${request.baseUrl}/${user.token}/check-out`;
-
-  const qrcodeCheckIn = await qrcode.toDataURL(checkInUrl);
-  const qrcodeCheckOut = await qrcode.toDataURL(checkOutUrl);
-
-  response.send(`
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <div class="container mx-auto px-4">
-      <h1 class="text-4xl py-4 text-center lg:text-left">${user.name}</h1>
-      <div class="lg:flex lg:space-x-10 space-y-10 lg:space-y-0">
-        <div class="text-center border border-gray-900 p-4 rounded">
-          <h2 class="text-2xl">Check-in</h2>
-          <img src="${qrcodeCheckIn}" alt="${user.name}" class="inline" />
-          <p>${checkInUrl}</p>
-        </div>
-        <div class="text-center border border-gray-900 p-4 rounded">
-          <h2 class="text-2xl">Check-out</h2>
-          <img src="${qrcodeCheckOut}" alt="${user.name}" class="inline" />
-          <p>${checkOutUrl}</p>
-        </div>
-      </div>
-    </div>
-  `);
 });
 
-router.get('/:token/check-in', async (request, response) => {
-  const user = await User.addHistory(request.params.token, 'CHECK_IN');
+router.get('/:token/check-in', async (request, response, next) => {
+  try {
+    const user = await User.addHistory(request.params.token, 'CHECK_IN');
 
-  if (!user) {
-    response.sendStatus(404);
-    return;
+    if (!user) {
+      response.sendStatus(404);
+      return;
+    }
+
+    response.render('users/check-in', {
+      emoji: emoji.get('heart'),
+    });
+  } catch (error) {
+    next(error);
   }
-
-  response.send(`
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <div class="container mx-auto p-4">
-      <div class="text-center text-2xl border border-gray-900 p-4 rounded">Check-in successful. Thank you ${emoji.get(
-        'heart',
-      )}.</div>
-    </div>
-  `);
 });
 
-router.get('/:token/check-out', async (request, response) => {
-  const user = await User.addHistory(request.params.token, 'CHECK_OUT');
+router.get('/:token/check-out', async (request, response, next) => {
+  try {
+    const user = await User.addHistory(request.params.token, 'CHECK_OUT');
 
-  if (!user) {
-    response.sendStatus(404);
-    return;
+    if (!user) {
+      response.sendStatus(404);
+      return;
+    }
+
+    response.render('users/check-out', {
+      emoji: emoji.get('heart'),
+    });
+  } catch (error) {
+    next(error);
   }
-
-  response.send(`
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <div class="container mx-auto p-4">
-      <div class="text-center text-2xl border border-gray-900 p-4 rounded">Check-out successful. Thank you ${emoji.get(
-        'heart',
-      )}.</div>
-    </div>
-  `);
 });
 
 export default router;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import helmet from 'helmet';
+import path from 'path';
 import pinoHttp from 'pino-http';
-import emoji from 'node-emoji';
 
 import { databaseUrl } from './helpers/database.js';
 import logger from './helpers/pino.js';
@@ -10,6 +10,9 @@ import controllerUsers from './controllers/users.js';
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', path.resolve('./src/views'));
+
 const pinoMiddleware = pinoHttp({
   logger,
 });
@@ -17,18 +20,20 @@ const pinoMiddleware = pinoHttp({
 app.use(helmet());
 app.use(pinoMiddleware);
 
-app.get('/', (request, response) => {
-  response.send(`
-    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
-    <div class="container mx-auto p-4">
-      <div class="text-center text-2xl border border-gray-900 p-4 rounded">Hello ${
-        emoji.random().emoji
-      }</div>
-    </div>
-  `);
-});
+app.get('/', (request, response) => response.render('index'));
 
 app.use('/users', controllerUsers);
+
+app.use((error, request, response, next) => {
+  logger.error(error);
+
+  response.status(500).json({
+    type: 'api_error',
+    message: error.message,
+  });
+
+  next();
+});
 
 app.listen(getEnvironment().PORT, () => {
   logger.info(`Server running on port ${getEnvironment().PORT}`);
